88211b7bf8d016cce3db816bdd64c845c43eaa5b
M Rizky Firdaus <punyaaris1@gmail.com>
2026-03-22 10:07:52 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,28 @@
+// === FILE PENANGKAP NOTIFIKASI BACKGROUND (SERVICE WORKER) ===
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDfsTAGclYUSRe6YhiljS-xb-v-9RBYVEM",
-    authDomain: "antriankcnew.firebaseapp.com",
-    projectId: "antriankcnew",
-    storageBucket: "antriankcnew.firebasestorage.app",
-    messagingSenderId: "348986439499",
-    appId: "1:348986439499:web:99578753dc9547d727dace"
+// KONEKSI KE DATABASE MLU
+const firebaseConfig = { 
+    apiKey: "AIzaSyAk3G5aQUBVe_g7_peVu1F6xllP_RejGq0", 
+    authDomain: "rtomaka-e67b5.firebaseapp.com", 
+    projectId: "rtomaka-e67b5", 
+    storageBucket: "rtomaka-e67b5.firebasestorage.app", 
+    messagingSenderId: "472036722228", 
+    appId: "1:472036722228:web:11d520f9204db317d02c61" 
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Mesin Penangkap Notifikasi di Latar Belakang (HP Mati / App Ditutup)
+// JIKA APLIKASI DITUTUP, MUNCULKAN POP-UP NOTIFIKASI DI LAYAR HP
 messaging.onBackgroundMessage(function(payload) {
+  console.log('Notif Masuk di Belakang Layar: ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/logo.png', // Pastikan Bapak punya file logo.png
-    badge: '/logo.png',
-    vibrate: [200, 100, 200]
+    icon: 'logo.png', // Logo MLU yang akan muncul di notif
+    vibrate: [200, 100, 200, 100, 200, 100, 200]
   };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  return self.registration.showNotification(notificationTitle, notificationOptions);
 });
