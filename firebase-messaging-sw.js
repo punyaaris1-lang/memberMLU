c2f16f9c0c249dbc660634046f82683404717c79
M Rizky Firdaus <punyaaris1@gmail.com>
2026-03-22 09:52:41 +0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,26 @@
-// file: firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
-// Konfigurasi Database MLU Bapak
 const firebaseConfig = {
-    apiKey: "AIzaSyAk3G5aQUBVe_g7_peVu1F6xllP_RejGq0",
-    authDomain: "rtomaka-e67b5.firebaseapp.com",
-    projectId: "rtomaka-e67b5",
-    storageBucket: "rtomaka-e67b5.firebasestorage.app",
-    messagingSenderId: "472036722228",
-    appId: "1:472036722228:web:11d520f9204db317d02c61"
+    apiKey: "AIzaSyDfsTAGclYUSRe6YhiljS-xb-v-9RBYVEM",
+    authDomain: "antriankcnew.firebaseapp.com",
+    projectId: "antriankcnew",
+    storageBucket: "antriankcnew.firebasestorage.app",
+    messagingSenderId: "348986439499",
+    appId: "1:348986439499:web:99578753dc9547d727dace"
 };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Menangkap Notifikasi saat Aplikasi Ditutup / Dilatarbelakangi
-messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Notifikasi Diterima: ', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'logomlu.png', // Ikon yang muncul di layar atas HP
-        badge: 'logomlu.png',
-        vibrate: [200, 100, 200, 100, 200, 100, 200] // Efek HP Getar panjang
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
+// Mesin Penangkap Notifikasi di Latar Belakang (HP Mati / App Ditutup)
+messaging.onBackgroundMessage(function(payload) {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/logo.png', // Pastikan Bapak punya file logo.png
+    badge: '/logo.png',
+    vibrate: [200, 100, 200]
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
