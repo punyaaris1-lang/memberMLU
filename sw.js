8e92b1e4a38a20f755abc557b5ab17fb1fbdf273
M Rizky Firdaus <punyaaris1@gmail.com>
2026-03-22 11:54:12 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Biarkan kosong dulu, ini hanya syarat wajib agar PWA bisa di-install
-});
